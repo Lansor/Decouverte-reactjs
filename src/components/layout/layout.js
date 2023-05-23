7d636393a824179363c8958a3bce387728df2dbc
Lansor <killian28100@gmail.com>
2023-05-23 11:16:59 +0200

    maj structure de la page

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,15 +1,31 @@
-const Layout = ({children}) => {
+import logo from '../../image.png'
+
+
+
+const Layout = ({ children }) => {
     return (
-        <>
-        <header>
-            <p>Je suis un header</p>
-        </header>
-        <main>
-            {children}
-        </main>
+        <div className='App'>
+            <header className='App-header'>
+                <img src={logo} className='App-logo' alt='logo' />
+                <p>
+                    Pour modifier la page c&#39;est ici :<code>src/App.js</code>.
+                </p>
+                <a
+                    className='App-link'
+                    href='https://reactjs.org'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                >
+                    Learn React
+                </a>
+            </header>
+            <main className='App-main'>
+                {children}
+            </main>
+            
         <footer>Killian Martins 2023</footer>
-        </>
+        </div>
     )
 }
 
-export default Layout;
+export default Layout
